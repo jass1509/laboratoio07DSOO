7bec7bb38632e137e77ab39f7634ccd1684f6b98
diegozc792-bit <dcervantesapa@unsa.edu.pe>
2025-11-06 09:19:44 -0500

    descripcion de herencia y evidencia de polimorfismo clase deposito

--- javaapplication46/Deposito.java
+++ javaapplication46/Deposito.java
@@ -2,11 +2,15 @@ package javaapplication46;
 import java.util.Date;
 
 public class Deposito extends Transaccion {
-
+	/*La clase Deposito extiende a la clase Transaccion, lo que significa
+	/que hereda todos los atributos y métodos públicos y protegidos de Transaccion.
+	/ Deposito es una 'Transaccion' especializada.*/
+	
     public Deposito(String codigo, double monto, Date fecha, Empleado empleado, Cuenta cuenta, String motivo) {
         super(codigo, monto, fecha, empleado, cuenta, motivo);
     }
 
+    //Polimorfismo
     @Override
     public boolean procesar(Cuenta cuentaRegistra) {
         cuentaRegistra.depositar(monto);
@@ -14,6 +18,8 @@ public class Deposito extends Transaccion {
         return true;
     }
 
+    
+    //Metodo string
     @Override
     public String toString() {
         return "[DEPÓSITO] Código: " + codigo +
