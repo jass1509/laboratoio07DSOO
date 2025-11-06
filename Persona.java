cca46d78a8e7bcb66ba91a53719dc147b7c18448
Justo Vilca Alessandro Josue <ajustov@unsa.edu.pe>
2025-11-06 00:44:30 -0500

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -1,13 +1,35 @@
-package javaapplication46;
-public class Persona {
-private String dni;
-    private String nombre;
-    private String apellido;
-    private String direccion;
-    private String telefono;
+import java.util.Date;
 
-    public void mostrarDatos() {
-        System.out.println(nombre + " " + apellido + " - DNI: " + dni);
+public class Persona {
+
+    protected String nombre;
+    protected String apellido;
+    protected String telefono;
+    protected String email;
+    protected Date fechaNacimiento;
+
+    public Persona(String nombre, String apellido, String telefono, String email, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
     }
-    
+
+    // Retorna nombre completo //
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+    // Actualiza teléfono y correo //
+    public void actualizarContacto(String telefono, String email) {
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    // Calcula edad aproximada según año //
+    public int calcularEdad() {
+        Date hoy = new Date();
+        return hoy.getYear() - fechaNacimiento.getYear();
+    }
 }
