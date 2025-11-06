ecbed54871fcb301ce5f2c104eb535531b2d792b
Jonnier Angel Condori Catasi <jcondoricata@unsa.edu.pe>
2025-11-05 23:02:03 -0500

    Extend Empleado class from Persona and add methods

--- Empleado.java
+++ Empleado.java
@@ -1,2 +1,13 @@
 package javaapplication46;
-public class Empleado {
+public class Empleado extends Persona {
+    private String idEmpleado;
+    private String cargo;
+
+    public void procesarTransaccion(Transaccion t) {
+        t.procesar();
+    }
+
+    public void registrarCliente(Cliente c) {
+        // lógica para registrar cliente
+    }
+}
