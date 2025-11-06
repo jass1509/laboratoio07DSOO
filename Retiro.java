d29f35fbb6f78ad1ae54c4659f02510b1f675224
Justo Vilca Alessandro Josue <ajustov@unsa.edu.pe>
2025-11-06 00:39:45 -0500

    Update Retiro.java

--- Retiro.java
+++ Retiro.java
@@ -3,28 +3,37 @@ import java.util.Date;
 public class Retiro extends Transaccion {
 
     private boolean limiteSuperado;
+    private String motivo;
 
-    public Retiro(String idTransaccion, double monto, Date fecha, Empleado empleadoGestor, Cuenta cuentaRegistra) {
+    public Retiro(String idTransaccion, double monto, Date fecha, Empleado empleadoGestor, Cuenta cuentaRegistra, String motivo) {
         super(idTransaccion, monto, fecha, empleadoGestor, cuentaRegistra);
+        this.motivo = motivo;
         this.limiteSuperado = false;
     }
 
-    // Procesa el retiro (solo si saldo suficiente)
+    // Resta saldo si no supera el límite y hay fondos //
     @Override
     public boolean procesar(Cuenta c) {
-        if (c == null) return false;
+        double limite = 1000;
 
-        if (c.getSaldo() >= monto) {
-            c.setSaldo(c.getSaldo() - monto);
-            return true;
-        } else {
+        if (monto > limite) {
             limiteSuperado = true;
             return false;
         }
+
+        boolean ok = c.retirar(monto);
+
+        if (ok) {
+            c.addMovimiento(this);
+        }
+
+        return ok;
     }
 
     @Override
     public String toString() {
-        return super.toString() + " | Exceso de retiro: " + (limiteSuperado ? "Sí" : "No");
+        return "Retiro | " + super.toString() +
+                " | Motivo: " + motivo +
+                " | Límite superado: " + limiteSuperado;
     }
 }
