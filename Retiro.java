f466b5249021a4859b9d0048d719691df3102875
Justo Vilca Alessandro Josue <ajustov@unsa.edu.pe>
2025-11-06 01:02:36 -0500

    Update Retiro.java

--- Retiro.java
+++ Retiro.java
@@ -1,9 +1,11 @@
 import java.util.Date;
 
+// Transacción de tipo Retiro
 public class Retiro extends Transaccion {
 
     private boolean limiteSuperado;
     private String motivo;
+    private double limite = 1000; // Límite simple permitido
 
     public Retiro(String idTransaccion, double monto, Date fecha, Empleado empleadoGestor, Cuenta cuentaRegistra, String motivo) {
         super(idTransaccion, monto, fecha, empleadoGestor, cuentaRegistra);
@@ -11,18 +13,20 @@ public class Retiro extends Transaccion {
         this.limiteSuperado = false;
     }
 
-    // Resta saldo si no supera el límite y hay fondos //
+    // Intenta retirar el monto si no supera el límite y hay saldo
     @Override
     public boolean procesar(Cuenta c) {
-        double limite = 1000;
 
+        // Verifica límite permitido
         if (monto > limite) {
             limiteSuperado = true;
             return false;
         }
 
+        // Se intenta retirar
         boolean ok = c.retirar(monto);
 
+        // Si se retiró correctamente, se registra
         if (ok) {
             c.addMovimiento(this);
         }
