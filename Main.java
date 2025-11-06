91d15dce42a92b47fb243e4fbdfa11654803ec5f
Justo Vilca Alessandro Josue <ajustov@unsa.edu.pe>
2025-11-06 01:55:46 -0500

    Update and rename main.java to Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Crear el menú principal del sistema
+        MenuSistema menu = new MenuSistema();
+        
+        menu.mostrarMenu();
+    }
+}
+
