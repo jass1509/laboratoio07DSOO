a717a6c5497251da4d8c0e82f2515acbf65f80fe
Justo Vilca Alessandro Josue <ajustov@unsa.edu.pe>
2025-11-06 03:39:13 -0500

    Update Transaccion.java

--- Transaccion.java
+++ Transaccion.java
@@ -1,36 +1,57 @@
 import java.util.Date;
 
 public abstract class Transaccion {
-
-    protected String idTransaccion;
+    protected String codigo;
     protected double monto;
     protected Date fecha;
-    protected Empleado empleadoGestor;
+    protected Empleado empleadoRegistra;
     protected Cuenta cuentaRegistra;
+    protected String motivo;
 
-    public Transaccion(String idTransaccion, double monto, Date fecha, Empleado empleadoGestor, Cuenta cuentaRegistra) {
-        this.idTransaccion = idTransaccion;
+    public Transaccion(String codigo, double monto, Date fecha, Empleado empleadoRegistra, Cuenta cuentaRegistra, String motivo) {
+        this.codigo = codigo;
         this.monto = monto;
         this.fecha = fecha;
-        this.empleadoGestor = empleadoGestor;
+        this.empleadoRegistra = empleadoRegistra;
         this.cuentaRegistra = cuentaRegistra;
+        this.motivo = motivo;
     }
 
-    // Devuelve el monto de la transacción
+    public abstract boolean procesar(Cuenta cuentaRegistra);
+
+    public String getCodigo() {
+        return codigo;
+    }
+
     public double getMonto() {
         return monto;
     }
 
-    // Método abstracto que se implementa según el tipo de transacción 
-    public abstract boolean procesar(Cuenta c);
+    public Date getFecha() {
+        return fecha;
+    }
 
-    // Retorna información para mostrar o registrar 
+    public Empleado getEmpleadoRegistra() {
+        return empleadoRegistra;
+    }
+
+    public Cuenta getCuentaRegistra() {
+        return cuentaRegistra;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
     @Override
     public String toString() {
-        return "ID: " + idTransaccion +
-                " | Monto: " + monto +
-                " | Fecha: " + fecha +
-                " | Gestor: " + (empleadoGestor != null ? empleadoGestor.getNombreCompleto() : "N/A") +
-                " | Cuenta: " + (cuentaRegistra != null ? cuentaRegistra.getNumeroCuenta() : "N/A");
+        return "Transacción{" +
+                "Código='" + codigo + '\'' +
+                ", Monto=" + monto +
+                ", Fecha=" + fecha +
+                ", Empleado='" + empleadoRegistra.getNombreCompleto() + '\'' +
+                ", Cuenta=" + cuentaRegistra.getNumeroCuenta() +
+                ", Motivo='" + motivo + '\'' +
+                '}';
     }
 }
