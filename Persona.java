c2b348502aa2acadca4e972b082f29becc237a4c
Jonnier Angel Condori Catasi <jcondoricata@unsa.edu.pe>
2025-11-05 23:01:21 -0500

    Add fields and method to Persona class

--- Persona.java
+++ Persona.java
@@ -1,5 +1,13 @@
 package javaapplication46;
 public class Persona {
+private String dni;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
 
+    public void mostrarDatos() {
+        System.out.println(nombre + " " + apellido + " - DNI: " + dni);
+    }
     
 }
